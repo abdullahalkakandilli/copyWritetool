8c2cc2cbd40435d0caa243163d10aae734864107
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-02-24 11:08:55 +0100

    openai key update

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-max_width_str = f"max-width: 1800px;"
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style,  unsafe_allow_html=True,
-)
+def _max_width_():
+    max_width_str = f"max-width: 1800px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
 
 
-#st.set_page_config(page_icon="images/icon.png", page_title="Copywriter Tool")
+st.set_page_config(page_icon="images/icon.png", page_title="Copywriter Tool")
 
 
 openai.api_key = st.sidebar.text_input(
